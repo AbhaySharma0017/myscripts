9bb1b7509807a1962867fc21b480ea2319be4a95
AbhaySharma0017 <abhaysharma0154@gmail.com>
2025-08-26 12:42:03 +0530

    Some more

--- 23_while_with_csv.sh
+++ 23_while_with_csv.sh
@@ -5,5 +5,13 @@ while IFS="," read Id Name Age
 do
     echo "Id is: $Id"
     echo "Name is: $Name"
-    echo "Age is: $Age"
+    #echo "Age is: $Age"
 done < test.csv
+
+# We can also do above task like below ways (In which frist line is not printed.)
+cat test.csv | awk 'NR!=1 {print}' | while IFS="," read Id Name Age
+do 
+    echo "Id is $Id"
+    echo "Name is $Name"
+    echo "Age is $Age"
+done <test.csv
